5f3e549335d77baefd4d9903303056e372fc5bd1
Ramon Rocha <ramon.rocha@compasso.com.br>
2023-02-16 14:39:47 -0300

    commit4

--- index.js
+++ index.js
@@ -3,51 +3,9 @@ const pulumi = require("@pulumi/pulumi");
 
 const { Resource } = require('twilio-pulumi-provider');
 
-
-const TaskqueueEUA = new Resource('taskqueue-english', {
-  resource: [
-    'taskrouter',
-    { workspaces: "WS6e2c9bd0401b0f7d881f38a761d31e39" },
-    'taskQueues',
-  ],
+const workspace = new Resource('example-workspace', {
+  resource: ['taskrouter', 'workspaces'],
   attributes: {
-    targetWorkers: `languages HAS "en"`,
-    friendlyName: 'Frances Queue',
+    friendlyName: 'New Workspace created with Pulumi',
   },
 });
-
-const TaskqueueSpani = new Resource('taskqueue-spanish', {
-  resource: [
-    'taskrouter',
-    { workspaces: "WS6e2c9bd0401b0f7d881f38a761d31e39" },
-    'taskQueues',
-  ],
-  attributes: {
-    targetWorkers: `languages HAS "es"`,
-    friendlyName: 'Germany Queue',
-  },
-});
-
-const worker1 = new Resource('worker-one', {
-    resource: [
-      'taskrouter',
-      { workspaces: "WS6e2c9bd0401b0f7d881f38a761d31e39" },
-      'workers',
-    ],
-    attributes: {
-      friendlyName: 'Ramon',
-      attributes: JSON.stringify({ languages: ['en', 'es'] }),
-    },
-  });
-  
-  const worker5 = new Resource('worker-two', {
-    resource: [
-      'taskrouter',
-      { workspaces: "WS6e2c9bd0401b0f7d881f38a761d31e39" },
-      'workers',
-    ],
-    attributes: {
-      friendlyName: 'Bob',
-      attributes: JSON.stringify({ languages: ['en'] }),
-    },
-  });
